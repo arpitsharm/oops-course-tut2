4b4c172f01d75fee50cff8e05720286735a00cc7
arpitsharm <150226804+arpitsharm@users.noreply.github.com>
2024-01-25 17:40:51 +0530

    Goods
    Good All for her

---
+++ oops_tut_2_basic_of_classes_and_object.py
@@ -0,0 +1,28 @@
+class Meta: # class
+    # def __init__(self, nameo) -> None: #assitent
+    #     print(f"Hey, {nameo}")
+
+    def __init__(self, nameo) -> None: #assitent
+        print("Hey, {}".format(nameo))
+
+        
+
+    def fun(self, name):
+        # print (f"Hello Kaise Hoo {name} Our Course Kaisa laga") #printting
+        return f"Hello Kaise Hoo {name} Our Course Kaisa laga"# return this
+
+    artibute = 100 # simple var
+
+    # assistent 
+    # def __init__(self, nameo) -> None: #assitent
+    #     print(f"Hey, {nameo}")
+    # assit kartha hoo
+
+
+
+
+# obj = Meta() # call my class
+obj = Meta("Rohan") # call my class
+# print(obj.artibute) # print the class on var named artibute
+# obj.fun("Rohan")
+# print(obj.fun("Rohan")) #return thing do the print()
